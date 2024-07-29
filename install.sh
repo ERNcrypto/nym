fc7e6790d229a8dfa54da1d91075da391719c7cb
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-07-29 22:17:28 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,6 +17,9 @@ if [ -f "$bash_profile" ]; then
     . $HOME/.bash_profile
 fi
 
+# Кратковременная пауза и выполнение скрипта, загружаемого с nodes.guru
+sleep 1 && curl -s https://api.nodes.guru/logo.sh | bash && sleep 1
+
 # Проверка, установлен ли node_id; если нет, запрос имени узла у пользователя
 if [ ! $node_id ]; then
 read -p "Введите имя узла: " node_id
@@ -46,14 +49,13 @@ cd $HOME
 rm -rf nym
 git clone https://github.com/nymtech/nym.git
 cd nym
-LATEST_RELEASE=$(curl -s "https://api.github.com/repos/nymtech/nym/releases" | grep '"tag_name": "nym-binaries' | awk -F '"' {'print $4'} | head -1)
+LATEST_RELEASE=$(curl -s "https://api.github.com/repos/nymtech/nym/releases" | grep '"tag_name":' | awk -F '"' {'print $4'} | head -1)
 git checkout $LATEST_RELEASE
-cargo build --release --bin nym-mixnode
-sudo mv target/release/nym-mixnode /usr/local/bin/
+cargo build --release --bin nym-node
+sudo mv target/release/nym-node /usr/local/bin/
 
-# Инициализация узла Nym и настройка брандмауэра
-# Убираем аргумент --host, так как он, похоже, больше не поддерживается
-nym-mixnode init --id $node_id
+# Инициализация узла Nym с использованием nym-node и настройка брандмауэра
+nym-node init --id $node_id
 sudo ufw allow 1789,1790,8000,22,80,443/tcp
 
 # Настройка хранения логов в systemd и перезагрузка systemd-journald
@@ -63,13 +65,13 @@ EOF
 sudo systemctl restart systemd-journald
 
 # Создание и настройка сервиса systemd для управления узлом Nym
-sudo tee <<EOF >/dev/null /etc/systemd/system/nym-mixnode.service
+sudo tee <<EOF >/dev/null /etc/systemd/system/nym-node.service
 [Unit]
-Description=Nym Mixnode
+Description=Nym Node
 
 [Service]
 User=$USER
-ExecStart=/usr/local/bin/nym-mixnode run --id '$node_id'
+ExecStart=/usr/local/bin/nym-node run --id '$node_id'
 KillSignal=SIGINT
 Restart=on-failure
 RestartSec=30
@@ -82,14 +84,14 @@ WantedBy=multi-user.target
 EOF
 sudo echo "DefaultLimitNOFILE=65535" >> /etc/systemd/system.conf
 sudo systemctl daemon-reload
-sudo systemctl enable nym-mixnode
-sudo systemctl restart nym-mixnode
+sudo systemctl enable nym-node
+sudo systemctl restart nym-node
 
 # Проверка статуса узла и вывод информации о состоянии установки
 echo -e '\n\e[42mПроверка состояния узла\e[0m\n' && sleep 1
-if service nym-mixnode status | grep -q "active (running)"; then
+if service nym-node status | grep -q "active (running)"; then
   echo -e "Ваш узел Nym \e[32mустановлен и работает\e[39m!"
-  echo -e "Вы можете проверить состояние узла с помощью команды \e[7mservice nym-mixnode status\e[0m"
+  echo -e "Вы можете проверить состояние узла с помощью команды \e[7mservice nym-node status\e[0m"
   echo -e "Нажмите \e[7mQ\e[0m для выхода из меню состояния"
 else
   echo -e "Ваш узел Nym \e[31mне был установлен корректно\e[39m, пожалуйста, переустановите."
