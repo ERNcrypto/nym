940931f433bfb3d974ef73aa38b16e30e922fbba
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-07-29 22:11:45 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,47 @@
-#!/bin/bash
+# Функция для проверки существования команды
 exists()
 {
   command -v "$1" >/dev/null 2>&1
 }
+
+# Проверка, установлен ли curl, и его установка, если он отсутствует
 if exists curl; then
 echo ''
 else
   sudo apt update && sudo apt install curl -y < "/dev/null"
 fi
+
+# Определение пути к .bash_profile и его загрузка, если файл существует
 bash_profile=$HOME/.bash_profile
 if [ -f "$bash_profile" ]; then
     . $HOME/.bash_profile
 fi
-sleep 1 && curl -s https://api.nodes.guru/logo.sh | bash && sleep 1
 
+# Проверка, установлен ли node_id; если нет, запрос имени узла у пользователя
 if [ ! $node_id ]; then
-read -p "Enter node name: " node_id
+read -p "Введите имя узла: " node_id
 echo 'export node_id='\"${node_id}\" >> $HOME/.bash_profile
 fi
+
+# Добавление source .bashrc в .bash_profile и его загрузка
 echo 'source $HOME/.bashrc' >> $HOME/.bash_profile
 . $HOME/.bash_profile
-echo 'Your node name: ' $node_id
+echo 'Имя вашего узла: ' $node_id
 
+# Обновление системы
 sudo apt update < "/dev/null"
 
+# Пауза и установка необходимых пакетов
 sleep 1
-
 sudo dpkg --configure -a
 sudo apt install ufw make clang pkg-config libssl-dev build-essential git -y -qq < "/dev/null"
+
+# Установка Rust
 sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 rustup update
+
+# Клонирование репозитория Nym и компиляция
 cd $HOME
 rm -rf nym
 git clone https://github.com/nymtech/nym.git
@@ -39,15 +50,19 @@ LATEST_RELEASE=$(curl -s "https://api.github.com/repos/nymtech/nym/releases" | g
 git checkout $LATEST_RELEASE
 cargo build --release --bin nym-mixnode
 sudo mv target/release/nym-mixnode /usr/local/bin/
-#nym-mixnode init --id $node_id --host $(curl ifconfig.me) --wallet-address $wallet
-nym-mixnode init --id $node_id --host $(curl ipinfo.io/ip)
+
+# Инициализация узла Nym и настройка брандмауэра
+# Убираем аргумент --host, так как он, похоже, больше не поддерживается
+nym-mixnode init --id $node_id
 sudo ufw allow 1789,1790,8000,22,80,443/tcp
-#sed -i.def "s|validator_api_urls.*|validator_api_urls = [\
-#	'https://validator.nymtech.net/api/'|" $HOME/.nym/mixnodes/*/config/config.toml
+
+# Настройка хранения логов в systemd и перезагрузка systemd-journald
 sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
 Storage=persistent
 EOF
 sudo systemctl restart systemd-journald
+
+# Создание и настройка сервиса systemd для управления узлом Nym
 sudo tee <<EOF >/dev/null /etc/systemd/system/nym-mixnode.service
 [Unit]
 Description=Nym Mixnode
@@ -69,11 +84,13 @@ sudo echo "DefaultLimitNOFILE=65535" >> /etc/systemd/system.conf
 sudo systemctl daemon-reload
 sudo systemctl enable nym-mixnode
 sudo systemctl restart nym-mixnode
-echo -e '\n\e[42mCheck node status\e[0m\n' && sleep 1
-if [[ service nym-mixnode status | grep active =~ "running" ]]; then
-  echo -e "Your Nym node \e[32minstalled and works\e[39m!"
-  echo -e "You can check node status by the command \e[7mservice nym-mixnode status\e[0m"
-  echo -e "Press \e[7mQ\e[0m for exit from status menu"
+
+# Проверка статуса узла и вывод информации о состоянии установки
+echo -e '\n\e[42mПроверка состояния узла\e[0m\n' && sleep 1
+if service nym-mixnode status | grep -q "active (running)"; then
+  echo -e "Ваш узел Nym \e[32mустановлен и работает\e[39m!"
+  echo -e "Вы можете проверить состояние узла с помощью команды \e[7mservice nym-mixnode status\e[0m"
+  echo -e "Нажмите \e[7mQ\e[0m для выхода из меню состояния"
 else
-  echo -e "Your Nym node \e[31mwas not installed correctly\e[39m, please reinstall."
+  echo -e "Ваш узел Nym \e[31mне был установлен корректно\e[39m, пожалуйста, переустановите."
 fi
